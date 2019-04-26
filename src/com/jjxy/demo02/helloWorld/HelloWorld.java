ed0c0fa7e5c11ca6a935e040fe110e4708a7b935
Your Name <you@example.com>
2019-04-26 23:48:50 +0800

    Initial commit

---
+++ src/com/jjxy/demo02/helloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.jjxy.demo02.helloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println();
+
+    }
+}
